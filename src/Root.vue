b177788fc66a236cb2a18e4c0250494cd927bd50
Ricardo Rossi <ricardo@endata.com>
2020-05-04 12:13:20 -0500

    init commit

---
+++ src/Root.vue
@@ -0,0 +1,82 @@
+<template>
+    <v-app id="inspire">
+        <v-navigation-drawer v-model="drawer" app clipped>
+            <v-list dense>
+                <v-list-item link>
+                    <v-list-item-action>
+                        <v-icon>tonality</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Dashboard</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-action>
+                        <v-icon>adjust</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Settings</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar app clipped-left>
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"/>
+            <v-toolbar-title>Application</v-toolbar-title>
+        </v-app-bar>
+
+        <v-content>
+            <v-container class="fill-height" fluid>
+                <v-row align="center" justify="center">
+                    <v-col class="shrink">
+                        <v-tooltip right>
+                            <template v-slot:activator="{ on }">
+                                <v-btn :href="source" icon large target="_blank" v-on="on">
+                                    <v-icon large>mdi-code-tags</v-icon>
+                                </v-btn>
+                            </template>
+                            <span>Source</span>
+                        </v-tooltip>
+                        <v-tooltip right>
+                            <template v-slot:activator="{ on }">
+                                <v-btn
+                                        icon
+                                        large
+                                        href="https://codepen.io/johnjleider/pen/bXNzZL"
+                                        target="_blank"
+                                        v-on="on"
+                                >
+                                    <v-icon large>mdi-codepen</v-icon>
+                                </v-btn>
+                            </template>
+                            <span>Codepen</span>
+                        </v-tooltip>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-content>
+
+        <v-footer app>
+            <span>&copy; 2020</span>
+        </v-footer>
+    </v-app>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+
+    export default Vue.extend({
+        name: 'Root',
+        props: {
+            source: String
+        },
+
+        data: () => ({
+            drawer: false
+        }),
+        created() {
+            this.$vuetify.theme.dark = true;
+        }
+    });
+</script>
